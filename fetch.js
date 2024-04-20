29a183d209b29d85e061ea971a726bb37e024665
Vedant Ganesh <>
2024-04-20 21:54:37 +0800

    Slightly improved version. Computes correct URI, but for some reason the code does not work.

--- fetch.js
+++ fetch.js
@@ -1,20 +1,35 @@
-function getFoodData(api_key, params) {					//params is a key:value list
+function promisedClick(listener) {
+	return new Promise ( (resolve) => {
+		document.removeEventListener("click", listener);
+		resolve();
+	});
+}
+
+function getFoodData(api_key, params) {								//params is a key:value list
 	const req = new XMLHttpRequest();
-	req.open("GET", "https://api.nal.usda.gov/fdc/v1/foods/search");
-	req.onload = () => {					//set up event listener (executes upon load)
-		if (req.readyState == 4 && xhr.status == 200) {
-			document.getElementById(output).innerHTML = req.response;		//spit out the response using DOM
+	const uri = "https://api.nal.usda.gov/fdc/v1/foods/search";
+	req.open("GET", uri);									//open request
+	req.onload = () => {									//set up event listener (executes upon load)
+		if (req.status == 200) {
+			document.getElementById("output").innerHTML = req.response;		//spit out the response using DOM
 		} 
-		else {
-			document.getElementById(output).innerHTML = `Error: ${req.status} \r\n ${req.response}`;
-												//if error, spit it out using DOM
+		else {										//403 (Forbidden)
+			console.log(`Error: ${req.status} \r\n ${req.response}`);		//spit out an error using the console
 		}
 	};
-	req.send(concat((params) => new URLSearchParams(params),`&api_key=${api_key}`));	//process key:value list and send to server
+	paramstr = new URLSearchParams(params);
+	reqstr = (uri.concat("?",`api_key=${api_key}`,"&",paramstr));	//process key:value list and send to server
+	console.log(reqstr);
+	//await promisedClick(document.addEventListener("click", null));				//wait for mouseclick in window
+	req.send(reqstr);
+	req.onreadystatechange = () => {
+		console.log(req.response);
+		console.log(req.responseText);
+	};
 }
 let key = "ifCZAscHCiFT5kcgxyDgDv6KW3dHzgnUIsvvFP4W";
 let parameter = {
-	query: "shredded cheese", 
+	query: "cheddar cheese", 
 	dataType: "Branded",
 	pageSize: 25,
 	pageNumber: 2,
