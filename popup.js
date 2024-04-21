1986d7f8f083c13c2ed24cfc262f40e74c49be3a
webwizarding <162193936+webwizarding@users.noreply.github.com>
2024-04-20 21:49:17 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,8 @@
-
+window.addEventListener('DOMContentLoaded', () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      files: ['content.js']
+    });
+  });
+});
